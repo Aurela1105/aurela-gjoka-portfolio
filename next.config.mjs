3b227b1a93e99c8b27af04e32fdd4304d67f9365
Aurela <ela11gjoka@gmail.com>
2026-08-26 15:37:40 +0200

    Configure Next.js for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+
+  basePath: "/aurela-gjoka-portfolio",
+  assetPrefix: "/aurela-gjoka-portfolio/",
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   images: {
     unoptimized: true,
   },
